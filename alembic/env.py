1fc7dd67377a5aaf64798a8ffb3ffa212707a6b1
rr3333mmAA <nice.amirkhan@gmail.com>
2025-04-11 15:21:08 +0200

    Added: setting custom database url support.

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,9 +6,17 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+DB_URL = os.getenv("DATABASE_URL")
+if DB_URL is not None:
+    config.set_main_option("sqlalchemy.url", DB_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
